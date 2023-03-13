662d68483239b83e38303615bb1ee4492642e901
Ozeyuta <ozeyuta.jkk@gmail.com>
2023-03-13 12:44:53 +0900

    フロント実装の準備

--- config/application.rb
+++ config/application.rb
@@ -18,8 +18,8 @@ module ChatApp
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
-      helper false
-      test_framework false
+      g.helper false
+      g.test_framework false
     end
   end
 end
